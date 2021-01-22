43d1e5f8556c12c2c2641c0e0b0ca9d12b92b411
root <alie.mughal@gmail.com>
2021-01-23 00:27:39 +0400

    chapter-6 completed

--- many_users.py
+++ many_users.py
@@ -11,8 +11,25 @@ if __name__=='__main__':
             'location':'Pakistan'
         },
     }
+    users1 = {
+        'Zubair': {
+            'first': 'Ali',
+            'last': 'Raza',
+            'location': 'Dubai'
+        },
+        'Hamza': {
+            'first': 'Muhammad',
+            'last': 'Mujtaba',
+            'location': 'Pakistan'
+        },
+    }
+    dic_list=[(k,v) for k,v in dict.items(users)]
+    print("dic convert to dic",dic_list)
+    for list in dic_list:
+        print("outer list",list)
+        for inlist in list:
+            print("inner list",inlist)
     for user, names in users.items():
-        print("user name \n"+user)
         full_name = names['first']+" "+names['last']
         location = names['location']
         print(full_name)
